3ec9e5d07e40dd658fc9a548ed58818e6d2bce91
Yixuan Huang <huang.yixua@northeastern.edu>
2022-12-03 22:46:42 -0500

    access location

--- app/src/main/java/edu/northeastern/fall22_team34/carpool/LoginActivity.java
+++ app/src/main/java/edu/northeastern/fall22_team34/carpool/LoginActivity.java
@@ -3,8 +3,15 @@ package edu.northeastern.fall22_team34.carpool;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,6 +24,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.io.Serializable;
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,6 +36,11 @@ public class LoginActivity extends AppCompatActivity {
     private FirebaseDatabase mDatabase;
 
     private List<String> currUsernames;
+
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
+    private Location currLocation;
 
 
     @Override
@@ -42,6 +55,26 @@ public class LoginActivity extends AppCompatActivity {
         EditText usernameEditText = findViewById(R.id.et_username);
         Button loginButton = findViewById(R.id.btn_login);
 
+        locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                currLocation = location;
+            }
+        };
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+        } else {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }
+
+        // listen to new users added
         mDatabase.getReference().child("users").addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
@@ -71,7 +104,7 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-
+        // create user profile with username and current location
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -81,7 +114,7 @@ public class LoginActivity extends AppCompatActivity {
                 } else {
                     String username = usernameEditText.getText().toString();
                     if (!currUsernames.contains(username)) {
-                        createUser(username);
+                        createUser(username, currLocation);
                     }
 
                     Intent homeActivity = new Intent(getApplicationContext(), HomeActivity.class);
@@ -93,8 +126,27 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
-    private void createUser(String username) {
-        User user = new User(username);
+    // add user to firebase
+    private void createUser(String username, Location currLocation) {
+        User user = new User(username, currLocation);
         mDatabase.getReference().child("users").child(username).setValue(user);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        locationManager.removeUpdates(locationListener);
     }
 }
--- app/src/main/java/edu/northeastern/fall22_team34/carpool/models/User.java
+++ app/src/main/java/edu/northeastern/fall22_team34/carpool/models/User.java
@@ -1,14 +1,21 @@
 package edu.northeastern.fall22_team34.carpool.models;
 
+import android.location.Location;
+
+import com.google.firebase.firestore.GeoPoint;
+
 public class User {
 
     public String username;
+    public GeoPoint currLocation;
+
 
     public User() {
 
     }
 
-    public User(String username) {
+    public User(String username, Location currLocation) {
         this.username = username;
+        this.currLocation = new GeoPoint(currLocation.getLatitude(), currLocation.getLongitude());
     }
 }
